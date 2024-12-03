62ba83f18d79ef0280bbac12882c8255749c00dd
DuaaNawwas <duaa.nawwas@gmail.com>
2024-12-03 22:09:48 +0300

    notifications

--- src/app/(app)/notifications/page.tsx
+++ src/app/(app)/notifications/page.tsx
@@ -1,10 +1,12 @@
 import { api } from "@/trpc/react";
-import { useState } from "react";
+import { format } from "date-fns";
+import { Dot } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
-  const [page, setPage] = useState(0);
-
-  const { data, fetchNextPage } =
+  const router = useRouter();
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     api.notification.getAllInfinite.useInfiniteQuery(
       {
         limit: 20,
@@ -13,20 +15,63 @@ export default function Page() {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
       },
     );
+  const observerRef = useRef<HTMLDivElement>(null);
 
-  const handleFetchNextPage = () => {
-    fetchNextPage();
-    setPage((prev) => prev + 1);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries?.[0]?.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1.0 },
+    );
+
+    if (observerRef.current) observer.observe(observerRef.current);
+
+    return () => {
+      if (observerRef.current) observer.unobserve(observerRef.current);
+    };
+  }, [observerRef, hasNextPage, fetchNextPage]);
+
+  const allData = data?.pages.flatMap((page) => page.notifications) ?? [];
+
+  const handleMarkAsViewed = (notificationId: string) => {
+    api.notification.markAsViewed
+      .useMutation({
+        onSuccess: () => {
+          router.push("/purchaseOrder/manage/" + notificationId);
+        },
+      })
+      .mutate({ notificationId });
   };
 
-  const toShow = data?.pages[page]?.notifications;
   return (
-    <div>
-      {toShow?.map((notification) => (
-        <div key={notification.notificationId}>
-          <p>{notification.text}</p>
+    <div className="flex w-[60%] flex-col items-center justify-center gap-2 px-20">
+      {allData.map((notification) => (
+        <div
+          key={notification.notificationId}
+          className={`${notification.viewed ? "bg-slate-50" : "bg-slate-300"} flex cursor-pointer flex-col gap-2 p-5`}
+          onClick={() => {
+            handleMarkAsViewed(notification.notificationId);
+          }}
+        >
+          <div className="flex justify-between">
+            <p>{notification.text}</p>
+            {!notification.viewed && <Dot size={28} />}
+          </div>
+          <p className="self-end text-xs text-gray-700">
+            {format(notification.createdAt, "h:mm a")}
+          </p>
         </div>
       ))}
+      <div ref={observerRef}>
+        {isFetchingNextPage
+          ? "Loading more..."
+          : hasNextPage
+            ? "Scroll down to load more"
+            : "No more data"}
+      </div>
     </div>
   );
 }
